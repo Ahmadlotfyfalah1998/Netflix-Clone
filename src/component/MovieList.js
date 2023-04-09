f6c7c365e7bf857753f1a978212441211ddc49ad
Ahmadlotfyfalah1998 <ahmad.lotfy.falah@gmail.com>
2023-04-09 23:38:31 +0000

    done

--- src/component/MovieList.js
+++ src/component/MovieList.js
@@ -12,7 +12,7 @@ export default function MovieList(props) {
                         <>
                             <SingleMovie movie={movie} />
 
-
+                            
 
                         </>
                     )
